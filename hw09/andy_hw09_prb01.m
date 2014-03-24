6241f536c38b190995ab8dddaa24b932816da5a9
Andy Reagan <github@andyreagan.com>
2014-03-24 16:48:43 -0400

    changed a la lakoba. not including all of the figures

--- hw09/andy_hw09_prb01.m
+++ hw09/andy_hw09_prb01.m
@@ -2,102 +2,44 @@
 %
 % solve BVP by collocation method
 
-Mvec = [10,15,20,30,40,60,80,100,120]';
-errorvec = zeros(size(Mvec));
-for j=1:length(Mvec)
-    %% setup
-    M = Mvec(j);
-    
-    % collocation points
-    x = linspace(0,1,M+2);
+M=10;
 
-    % set r
-    r = ((2.*x(2:end-1)-4)./((1+x(2:end-1)).^2))';
-    
-    % build A
-    % A = sparse(M,M);
-    A = zeros(M,M);
-    for i=1:M
-        A(:,i) = -pi^2*i^2.*sin(i*pi.*x(2:end-1)) + (-2./((1+x(2:end-1)).^2)).*sin(i*pi.*x(2:end-1));
-    end
-    
-    %% solve
-    c = A\r;
-    
-    %% add all of the functions
-    y = x';
-    for i=1:M
-        y = y+c(i).*sin(i*pi.*x');
-    end
-    
-    yexact = 2.*x./(1+x);
-    
-    %% quick plot of all of the theta's
-    figure;
-    for i=1:M
-        plot(x,c(i).*sin(i*pi.*x'));
-        hold on;
-    end
-    
-    %% compute max error (and save)
-    maxerror = max(abs(yexact'-y));
-    fprintf('maximum error for M = %g is %g\n',M,maxerror);
-    errorvec(j) = maxerror;
-    
-    %%  plot the solution, and exact
-    figure;
-    tmpfigh = gcf;
-    clf;
-    figshape(600,600);
-    set(gcf,'Color','none');
-    set(gcf,'InvertHardCopy', 'off');
-    set(gcf,'DefaultAxesFontname','helvetica');
-    set(gcf,'DefaultLineColor','r');
-    set(gcf,'DefaultAxesColor','none');
-    set(gcf,'DefaultLineMarkerSize',5);
-    set(gcf,'DefaultLineMarkerEdgeColor','k');
-    set(gcf,'DefaultLineMarkerFaceColor','g');
-    set(gcf,'DefaultAxesLineWidth',0.5);
-    set(gcf,'PaperPositionMode','auto');
-    
-    plot(x',y,'LineWidth',2,'Color','b');
-    hold on;
-    plot(x,yexact,'LineWidth',2,'Color','r')
-    legend({'numerical','exact'},'Location','NorthWest');
-    legend boxoff;
-    set(gca, 'fontsize',18)
-    xlabel('x','FontSize',20)
-    ylabel('y','FontSize',20)
-    
-    psprintcpdf_keeppostscript(sprintf('andy_hw09_prb01_%02g_m%02g',1,M));
-    
-    %%  plot the solution error
-    figure;
-    tmpfigh = gcf;
-    clf;
-    figshape(600,600);
-    set(gcf,'Color','none');
-    set(gcf,'InvertHardCopy', 'off');
-    set(gcf,'DefaultAxesFontname','helvetica');
-    set(gcf,'DefaultLineColor','r');
-    set(gcf,'DefaultAxesColor','none');
-    set(gcf,'DefaultLineMarkerSize',5);
-    set(gcf,'DefaultLineMarkerEdgeColor','k');
-    set(gcf,'DefaultLineMarkerFaceColor','g');
-    set(gcf,'DefaultAxesLineWidth',0.5);
-    set(gcf,'PaperPositionMode','auto');
-    
-    plot(x,abs(yexact-y'),'LineWidth',2,'Color','r')
-    set(gca, 'fontsize',18)
-    xlabel('x','FontSize',20)
-    ylabel('y','FontSize',20)
-    
-    psprintcpdf_keeppostscript(sprintf('andy_hw09_prb01_%02g_m%02g_error',1,M));
-    close all;
+% collocation points
+x = linspace(0,1,M+2);
+
+% set r
+r = ((2.*x(2:end-1)-4)./((1+x(2:end-1)).^2))';
+
+% build A
+% A = sparse(M,M);
+A = zeros(M,M);
+for i=1:M
+    A(:,i) = -pi^2*i^2.*sin(i*pi.*x(2:end-1)) + (-2./((1+x(2:end-1)).^2)).*sin(i*pi.*x(2:end-1));
 end
 
-%% plot error versus 1/M
+%% solve
+c = A\r;
 
+%% add all of the functions
+y = x';
+for i=1:M
+    y = y+c(i).*sin(i*pi.*x');
+end
+
+yexact = 2.*x./(1+x);
+
+%% quick plot of all of the theta's
+figure;
+for i=1:M
+    plot(x,c(i).*sin(i*pi.*x'));
+    hold on;
+end
+
+%% compute max error (and save)
+maxerror = max(abs(yexact'-y));
+fprintf('maximum error for M = %g is %g\n',M,maxerror);
+
+%%  plot the solution, and exact
 figure;
 tmpfigh = gcf;
 clf;
@@ -113,12 +55,57 @@ set(gcf,'DefaultLineMarkerFaceColor','g');
 set(gcf,'DefaultAxesLineWidth',0.5);
 set(gcf,'PaperPositionMode','auto');
 
-plot(1./Mvec,errorvec,'LineWidth',2,'Color','b')
-
+plot(x',y,'LineWidth',2,'Color','b');
+hold on;
+plot(x,yexact,'LineWidth',2,'Color','r')
+legend({'numerical','exact'},'Location','NorthWest');
+legend boxoff;
 set(gca, 'fontsize',18)
-xlabel('1/M','FontSize',20)
-ylabel('Max error','FontSize',20)
+xlabel('x','FontSize',20)
+ylabel('y','FontSize',20)
+title(sprintf('numerical versus exact solution, M = %g',M));
 
-psprintcpdf_keeppostscript(sprintf('andy_hw09_prb01_%02g',2));
+%%  plot the solution error
+figure;
+tmpfigh = gcf;
+clf;
+figshape(600,600);
+set(gcf,'Color','none');
+set(gcf,'InvertHardCopy', 'off');
+set(gcf,'DefaultAxesFontname','helvetica');
+set(gcf,'DefaultLineColor','r');
+set(gcf,'DefaultAxesColor','none');
+set(gcf,'DefaultLineMarkerSize',5);
+set(gcf,'DefaultLineMarkerEdgeColor','k');
+set(gcf,'DefaultLineMarkerFaceColor','g');
+set(gcf,'DefaultAxesLineWidth',0.5);
+set(gcf,'PaperPositionMode','auto');
 
-% close all;
+plot(x,abs(yexact-y'),'LineWidth',2,'Color','r')
+set(gca, 'fontsize',18)
+xlabel('x','FontSize',20)
+ylabel('y','FontSize',20)
+title(sprintf('error of solution, M = %g',M));
+
+% %% plot error versus 1/M
+%
+% figure;
+% tmpfigh = gcf;
+% clf;
+% figshape(600,600);
+% set(gcf,'Color','none');
+% set(gcf,'InvertHardCopy', 'off');
+% set(gcf,'DefaultAxesFontname','helvetica');
+% set(gcf,'DefaultLineColor','r');
+% set(gcf,'DefaultAxesColor','none');
+% set(gcf,'DefaultLineMarkerSize',5);
+% set(gcf,'DefaultLineMarkerEdgeColor','k');
+% set(gcf,'DefaultLineMarkerFaceColor','g');
+% set(gcf,'DefaultAxesLineWidth',0.5);
+% set(gcf,'PaperPositionMode','auto');
+%
+% plot(1./Mvec,errorvec,'LineWidth',2,'Color','b')
+%
+% set(gca, 'fontsize',18)
+% xlabel('1/M','FontSize',20)
+% ylabel('Max error','FontSize',20)
