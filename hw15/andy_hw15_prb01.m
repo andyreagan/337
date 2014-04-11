91419b11fe64b15db681ec0ad02e63788d5f3109
Andy Reagan <github@andyreagan.com>
2014-04-10 23:02:00 -0400

    worked through assigned problems

---
+++ hw15/andy_hw15_prb01.m
@@ -0,0 +1,94 @@
+% HW15 Problem 1
+%
+% Solve the 2D IBVP using the simple explicit method
+
+%% parameters
+pb = 1; % whether to plot intermediate solution
+t0 = 0.0;
+tf = 0.5;
+h = 0.1; % one at a time!
+k = 0.002;
+r = k/h^2;
+% domain
+ymax = 2.5;
+y = (0:h:ymax)';
+ymids = y(2:end-1);
+x = (0:h:1)';
+xmids = x(2:end-1);
+M = length(x); 
+N = length(y);
+t = (t0:k:tf)';
+% initial distribution
+[xx,yy] = meshgrid(x,y);
+u0 = sin(pi.*xx).*sin(pi.*yy./ymax);
+% boundaries
+g00 = 0.*ones(length(t),1); % x at 0
+g10 = 0.*ones(length(t),1); % x at 1
+g01 = 0.*ones(length(t),1); % y at 0
+g11 = 0.*ones(length(t),1); % y at ymax
+
+u = u0;
+
+%% solving loop
+
+if pb
+    figure(14040101); % for watching solution
+    mesh(u0);
+    xlabel('y','FontSize',16);
+    ylabel('x','FontSize',16);
+    zlabel('u','FontSize',16);
+    zlim([0,1]);
+end
+
+for i=2:length(t)
+    disp(t(i));
+    % first set the size of unew
+    unew = zeros(N,M);
+    % take the middle of the matrix
+    umid = u(2:end-1,2:end-1);
+    % update
+    % note that the index is (y,x)
+    % such that (1:end-2,2:end-1) grabs from 0 boundary in y
+    unew(2:end-1,2:end-1) = (1-4*r).*umid...
+        +r.*u(1:end-2,2:end-1)+r.*u(3:end,2:end-1)... % shifted in y
+        +r.*u(2:end-1,1:end-2)+r.*u(2:end-1,3:end); % shifted in x
+    
+    % check minimax prinicple
+    % if max(unew) > max(u0)
+    %     fprintf('your solution is behaving badly\n')
+    %     break
+    % end
+    
+    u = unew;
+    
+    if pb
+        figure(14040101); % for watching solution
+        mesh(yy,xx,u);
+        zlim([0,1]);
+        xlabel('y','FontSize',16);
+        ylabel('x','FontSize',16);
+        zlabel('u','FontSize',16);
+        % pause(.1);
+    end
+end
+
+
+%% plotting
+figure(14040201);
+clf;
+mesh(yy,xx,u);
+xlabel('y','FontSize',16);
+ylabel('x','FontSize',16);
+zlabel('u','FontSize',16);
+
+uexact = sin(pi.*xx).*sin(pi.*yy./ymax).*exp(-(1+1/ymax^2)*(pi^2*tf));
+
+% compare to exact
+figure(14040301);
+clf;
+mesh(yy,xx,uexact-u);
+xlabel('y','FontSize',16);
+ylabel('x','FontSize',16);
+zlabel('u','FontSize',16);
+
+
---
+++ hw15/andy_hw15_prb02.m
@@ -0,0 +1,94 @@
+% HW15 Problem 1
+%
+% Solve the 2D IBVP using the simple explicit method
+
+%% parameters
+pb = 1; % whether to plot intermediate solution
+t0 = 0.0;
+tf = 0.5;
+h = 0.1; % one at a time!
+k = 0.002;
+r = k/h^2;
+% domain
+ymax = 2.5;
+y = (0:h:ymax)';
+x = (0:h:1)';
+M = length(x); 
+N = length(y);
+t = (t0:k:tf)';
+% initial distribution
+[xx,yy] = meshgrid(x,y);
+u0 = 10.*sin(pi.*xx).*sin(pi.*yy./ymax)+sin(2*pi.*yy./ymax).*(1-xx)+cos(2*pi.*yy./ymax).*xx;
+% boundaries
+g00 = @(t) sin(2*pi.*y./ymax).*exp(-(2*pi/ymax)^2*t); % x at 0
+g10 = @(t) cos(2*pi.*y./ymax).*exp(-(2*pi/ymax)^2*t); % x at 1
+g01 = @(t) x.*exp(-(2*pi/ymax)^2*t); % y at 0
+g11 = @(t) x.*exp(-(2*pi/ymax)^2*t); % y at ymax
+
+u = u0;
+
+%% solving loop
+
+if pb
+    figure(15020101); % for watching solution
+    mesh(yy,xx,u0);
+    xlabel('y','FontSize',16);
+    ylabel('x','FontSize',16);
+    zlabel('u','FontSize',16);
+    zlim([0,max(max(u0))]);
+end
+
+for i=2:length(t)
+    disp(t(i));
+    % first set the size of unew
+    unew = zeros(N,M);
+    % take the middle of the matrix
+    umid = u(2:end-1,2:end-1);
+    % update
+    % note that the index is (y,x)
+    % such that (1:end-2,2:end-1) grabs from 0 boundary in y
+    unew(2:end-1,2:end-1) = (1-4*r).*umid...
+        +r.*u(1:end-2,2:end-1)+r.*u(3:end,2:end-1)... % shifted in y
+        +r.*u(2:end-1,1:end-2)+r.*u(2:end-1,3:end); % shifted in x
+    unew(:,1) = g00(t(i));
+    unew(:,end) = g10(t(i));
+    unew(1,:) = g01(t(i));
+    unew(end,:) = g11(t(i));
+    
+    % check minimax prinicple
+    % if max(unew) > max(u0)
+    %     fprintf('your solution is behaving badly\n')
+    %     break
+    % end
+    
+    u = unew;
+    
+    if pb
+        figure(15020101); % for watching solution
+        mesh(yy,xx,u);
+        zlim([0,max(max(u0))]);
+        xlabel('y','FontSize',16);
+        ylabel('x','FontSize',16);
+        zlabel('u','FontSize',16);
+        pause(.01);
+    end
+end
+
+
+%% plotting
+figure(15020201);
+clf;
+mesh(yy,xx,u);
+xlabel('y','FontSize',16);
+ylabel('x','FontSize',16);
+zlabel('u','FontSize',16);
+
+uexact = 10.*sin(pi.*xx).*sin(pi.*yy./ymax).*exp(-(1+1/ymax^2)*pi^2*tf)+(sin(2*pi.*yy./ymax).*(1-xx)+cos(2*pi.*yy./ymax).*xx).*exp(-(2*pi/ymax)^2*tf);y
+
+% compare to exact
+figure(15020301);
+clf;
+mesh(yy,xx,uexact-u);
+xlabel('y','FontSize',16);
+ylabel('x','FontSize',16);
+zlabel('u','FontSize',16);
